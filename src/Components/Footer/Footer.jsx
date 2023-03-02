d9186b7041beed4bd76e4d5d9df55f7da5094a77
AdsonVilla <adson23_@hotmail.com>
2023-03-02 17:50:10 -0300

    ajuste inicial do footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,28 +1,40 @@
+import {
+  AiOutlineInstagram,
+  AiOutlineMail,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
 import "./Footer.css";
 
 export function Footer() {
   return (
     <footer>
       <div className="footer-container">
-        <div className="social-list">
-          <ul>
-            <li className="item">
-              <a href="#">
-                <img></img> Instagram
-              </a>
-            </li>
-            <li className="item">
-              <a href="#">
-                <img></img> E-mail
-              </a>
-            </li>
-            <li className="item">
-              <a href="#">
-                <img></img> WhatsApp
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="social-list">
+          <li className="item">
+            <a href="#">
+              <div className="social">
+                <AiOutlineInstagram size={24} color="#fff" />
+                Instagram
+              </div>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#">
+              <div className="social">
+                <AiOutlineMail size={24} color="#fff" />
+                E-mail
+              </div>
+            </a>
+          </li>
+          <li className="item">
+            <a href="#">
+              <div className="social">
+                <AiOutlineWhatsApp size={24} color="#fff" />
+                WhatsApp
+              </div>
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
