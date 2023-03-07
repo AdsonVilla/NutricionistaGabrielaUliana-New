046c377b66f9cd8485158ae0cb36b9546865e922
AdsonVilla <adson23_@hotmail.com>
2023-03-07 15:00:21 -0300

    Adição da seção vantagens e controle de responsividade

--- src/Components/Advantages/Advantages.jsx
+++ src/Components/Advantages/Advantages.jsx
@@ -2,27 +2,29 @@ import "./Advantages.css";
 
 export function Advantages() {
   return (
-    <div class="advantages">
-      <div class="advantage">
-        <h3>Nutrição sem restrição</h3>
-        <p>
-          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
-          dolor sit amet.
-        </p>
-      </div>
-      <div class="advantage">
-        <h3>Corpo e mente</h3>
-        <p>
-          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
-          dolor sit amet.
-        </p>
-      </div>
-      <div class="advantage">
-        <h3>Algo mais</h3>
-        <p>
-          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
-          dolor sit amet.
-        </p>
+    <div className="advantages-section">
+      <div class="advantages">
+        <div class="advantage">
+          <h3>Nutrição sem restrição</h3>
+          <p>
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet.
+          </p>
+        </div>
+        <div class="advantage">
+          <h3>Corpo e mente</h3>
+          <p>
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet.
+          </p>
+        </div>
+        <div class="advantage">
+          <h3>Algo mais</h3>
+          <p>
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet.
+          </p>
+        </div>
       </div>
     </div>
   );
